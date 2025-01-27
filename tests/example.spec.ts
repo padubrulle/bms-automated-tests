3c075a602d5bf904468e96032beb8b3faed882a7
Pierre-Alain Dubrulle <paldubrulle@partner.auchan.fr>
2025-01-27 14:48:31 +0100

    First test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('change to skill page', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await page.getByText('Liste des compétences').click();
+
 });
 
 test('get started link', async ({ page }) => {
